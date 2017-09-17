30d57379ba265e488cf4422b9033dd3e6c150680
Shannin Ciprich <shanninc@gmail.com>
2017-09-17 09:08:22 -0400

    add some error handling

--- index.js
+++ index.js
@@ -17,10 +17,10 @@ function requestTopPackages(callback) {
     }, function(error, response, body) {
         var handler = new htmlparser.DefaultHandler(function (error, dom) {
             if (error) {
-                callback(null, 'There was a problem parsing the webpage')
+                callback([], 'There was a problem parsing the webpage')
             }
 
-            parseTopPackages(dom)
+            parseTopPackages(dom, callback)
         });
 
         var parser = new htmlparser.Parser(handler)
@@ -28,16 +28,21 @@ function requestTopPackages(callback) {
     });
 }
 
-function parseTopPackages(dom) {
-    var items = []
-
+function parseTopPackages(dom, callback) {
     var listElements = select(dom, '.package-details h3 a')
 
-    listElements.forEach(function (element) {
-        var name = element.children[0]
+    if (listElements && listElements.length > 0) {
+        var items = []
 
-        console.log(name.raw)
-    })
+        listElements.forEach(function (element) {
+            var name = element.children[0]
+            items.push(name.raw)
+        })
+
+        callback(items, null)
+    } else {
+        callback([], 'No packages found')
+    }
 }
 
 
@@ -45,7 +50,9 @@ function parseTopPackages(dom) {
 
 
 function downloadPackages (count, callback) {
-
+    requestTopPackages(function(list, error) {
+        console.log(list)
+    })
 }
 
 module.exports = downloadPackages
